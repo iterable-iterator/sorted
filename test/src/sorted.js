2d653fe52843355da944c865f7edafeed54afe8c
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-04-30 17:48:01 +0200

    :construction: progress: Import existing sources and tests.

---
+++ test/src/sorted.js
@@ -0,0 +1,34 @@
+import test from 'ava';
+
+import {sorted} from '../../src/index.js';
+import * as primitive from '@total-order/primitive';
+import {tee} from '@aureooms/js-itertools';
+
+const increasing = (a, b) => primitive.increasing(a, b);
+const decreasing = (a, b) => primitive.decreasing(a, b);
+
+const macro = (t, compare, array) => {
+	const [reference, input] = tee(array, 2);
+
+	const output = sorted(compare, input);
+	const expected = Array.from(reference).sort(compare);
+
+	t.not(output, input, 'check that output is not the same object as input');
+	t.is(output.length, expected.length, 'length check');
+	t.deepEqual(output, expected, 'check that output is as expected');
+};
+
+macro.title = (title, compare, array) =>
+	title ?? `sorted(${compare.name}, ${JSON.stringify(array)})`;
+
+for (const compare of [increasing, decreasing]) {
+	const array = [];
+
+	const n = 100;
+
+	for (let i = n; i--; ) {
+		array.push(Math.random());
+	}
+
+	test(macro, compare, array);
+}
